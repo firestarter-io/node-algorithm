ce5cc58536591be22fea15df7d6fa77bebe214ec
slutske22 <seth.lutske@gmail.com>
2021-01-04 21:52:40 -0800

    comments

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,22 @@
 import { tiles } from './data';
 
 export const scale: number = 12;
+export const tileCache = tiles;
+
+/**
+ * Saves tile image data to specified storage location
+ * @param {String} tileName | Name of tile in the format of X<X>Y<Y>Z<Z>
+ * @param {ImageData} tileData | ImageData for tile
+ */
 export const saveTile = (tileName: string, tileData: ImageData): void => {
 	tiles[tileName] = tileData;
 };
+
+/**
+ * Given the tile name, retrieves the tile ImageData
+ * @param {String} tileName | Name of tile in the format of X<X>Y<Y>Z<Z>
+ */
 export const retrieveTile = (tileName: string): ImageData => tiles[tileName];
-export const tileCache = tiles;
 
 export default {
 	scale,
