e9f5f88f0799234984d0cb679b0bb098dad62031
slutske22 <seth.lutske@gmail.com>
2021-01-09 21:57:46 -0800

    esri image fetching util class

--- src/controllers/campaign.ts
+++ src/controllers/campaign.ts
@@ -1,23 +1,37 @@
 // POST /api/campaign
 
+import fetch from 'node-fetch';
 import * as L from 'leaflet';
 import { createDEM } from '../core/getdata/createDEM';
 import { getTopography } from '../core/getData/getTopography';
+import { EsriImageRequest } from '../core/utils/geometry/esri-utils';
 
 export const campaign = async (req, res) => {
 	const { mapBounds, latlng, zoom } = req.body;
 
 	// Get DEM tiles for map bounds
-	mapBounds && createDEM(mapBounds);
-	const topo = await getTopography(latlng);
-	latlng && console.log(new L.LatLng(latlng.lat, latlng.lng));
-	latlng && zoom && console.log(topo);
+	// mapBounds && createDEM(mapBounds);
+	// const topo = await getTopography(latlng);
+	// latlng && console.log(new L.LatLng(latlng.lat, latlng.lng));
+	// latlng && zoom && console.log(topo);
 
 	// Pseudocode for new campaign logic flow
 	// process req.body
 	// .then(preload all data for initial bounds)
 	// .then(calculate fire spread step by step)
 	// .then(return response to client)
+
+	// fetch(
+	// 	'https://landsat.arcgis.com/arcgis/rest/services/Landsat/PS/ImageServer/?f=json'
+	// )
+	// 	.then((res) => res.json())
+	// 	.then((r) => console.log(r));
+
+	const satelliteRequest = new EsriImageRequest(
+		'https://landsat.arcgis.com/arcgis/rest/services/Landsat/PS/ImageServer/?f=json'
+	);
+
+	satelliteRequest.fetchImage();
 
 	res.send('good job ahole');
 };
--- src/core/utils/geometry/esri-utils.ts
+++ src/core/utils/geometry/esri-utils.ts
@@ -1,4 +1,8 @@
+import fetch from 'node-fetch';
+import { Bounds } from 'leaflet';
+import { scale } from '../../../config';
 import { MapBounds } from '../../../types/gis.types';
+import { project, unproject } from './projections';
 
 export function boundsToExtent(bounds: MapBounds) {
 	return {
@@ -10,4 +14,48 @@ export function boundsToExtent(bounds: MapBounds) {
 			wkid: 4326,
 		},
 	};
+}
+
+// https://github.com/Esri/esri-leaflet/blob/5569b703ed9ab2aeb83d57cb55cd1bc940fea38f/src/Layers/RasterLayer.js
+export function calculateBbox(pixelBounds) {
+	var sw = unproject(pixelBounds.getBottomLeft(), scale);
+	var ne = unproject(pixelBounds.getTopRight(), scale);
+
+	var neProjected = project(ne, scale);
+	var swProjected = project(sw, scale);
+
+	// this ensures ne/sw are switched in polar maps where north/top bottom/south is inverted
+	//  var boundsProjected = bounds(neProjected, swProjected);
+
+	//  return [boundsProjected.getBottomLeft().x, boundsProjected.getBottomLeft().y, boundsProjected.getTopRight().x, boundsProjected.getTopRight().y].join(',');
+}
+
+/**
+ * Util class for creating esri image requests
+ */
+export class EsriImageRequest {
+	_url: string;
+	_bounds: Bounds;
+	_layerJSON: any;
+
+	constructor(url: string, bounds?: Bounds) {
+		this._url = url;
+		this._bounds = bounds;
+	}
+
+	// fetch layer JSON and store in instance
+	async _fetchJson() {
+		const response = await fetch(this._url);
+		const esriJSON = await response.json();
+		this._layerJSON = esriJSON;
+		return esriJSON;
+	}
+
+	// request image based on bounds
+	async fetchImage() {
+		if (!this._layerJSON) {
+			await this._fetchJson();
+		}
+		console.log(this._layerJSON);
+	}
 }
