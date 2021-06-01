6d50a6f3933fe3212388fb4cdbea010b23cb0457
slutske22 <seth.lutske@gmail.com>
2021-06-01 13:48:48 -0700

    Comments and cleanup:

--- src/core/burncode/Timestep.ts
+++ src/core/burncode/Timestep.ts
@@ -32,7 +32,7 @@ class TimeStep {
 	 */
 	burnMatrices: Matrix[];
 	/**
-	 * Cells that have been calculated in this timestep
+	 * Ids of Cells that have been calculated in this timestep
 	 */
 	touchedCells: Set<string> = new Set<string>();
 	/**
@@ -41,8 +41,11 @@ class TimeStep {
 	events: FireStarterEvent[];
 
 	/**
-	 * A TimeStep is a snapshot of the state of a Campaign at a given time.
-	 * Contains unique information for a given timestep, like the state of the burnMatrices,
+	 * A TimeStep produces a snapshot of the state of a Campaign at a given time.
+	 * The calculations that determine how a fire will progress from one moment to another
+	 * are contained in the Timestep's methods.
+	 *
+	 * Returns unique information for a given timestep, like the state of the burnMatrices,
 	 * their derived polygons, any events for the timestep, etc.
 	 * @param campaign | The Campaign that the timestep belongs to
 	 */
