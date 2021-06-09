a3c3b639b194e7ee594bc5909a603081957c5bd5
slutske22 <seth.lutske@gmail.com>
2021-06-08 23:01:40 -0700

    Alpha wind

--- src/core/burncode/formulas.ts
+++ src/core/burncode/formulas.ts
@@ -26,19 +26,40 @@ const probabilityOfIgnition = (
 };
 
 /**
- * Function which determines the alpha wind factor, which may positively or negatively
+ * Function which determines the alpha slope factor, which may positively or negatively
  * affect the burn probability of a cell.  Reverse engineered from figure 6 of
  * ["PROPAGATOR: An Operational Cellular-Automata Based Wildfire Simulator"](https://www.mdpi.com/2571-6255/3/3/26/htm)
  *
  * google "plot .5 + 0.75 * 1 /(e^(-(x^3 / 30000)) + 0.5 ) from -150 to 150"
  *
  * Could use some fine tuning
- * @param slope Numer, slope, in percent
+ * @param slope slope, in percent
  */
 const alphaSlope = (slope: number) => {
 	return (
 		0.5 + 0.75 * (1 / (Math.pow(Math.E, -Math.pow(slope, 3) / 3000) + 0.5))
 	);
+};
+
+/**
+ * Function which determines the alpha wind factor, which may positively or negatively
+ * affect the burn probability of a cell.  Reverse engineered from figure 7 of
+ * ["PROPAGATOR: An Operational Cellular-Automata Based Wildfire Simulator"](https://www.mdpi.com/2571-6255/3/3/26/htm)
+ *
+ * Describes a group of equations which each approximate a normal distribution centered at x = 0
+ *
+ * @param direction Wind direction i degrees, relative to angle from Cell to NeighborCell
+ * @param speed Wind speed in km/h
+ */
+const alphaWind = (direction: number, speed: number) => {
+	if (speed === 0) {
+		return 1;
+	}
+	const s = speed / 100;
+	const yShift = 1 - s;
+	const amplitude = 1 + 3 * s;
+	const spread = Math.pow(10, speed / 100) / 100;
+	return amplitude / Math.pow(Math.E, (spread * direction) ** 2) + yShift;
 };
 
 /**
