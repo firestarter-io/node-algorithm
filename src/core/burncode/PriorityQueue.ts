1668d43221e8fdc321ab5d996896978aa6e0891f
slutske22 <seth.lutske@gmail.com>
2021-08-30 15:13:21 -0700

    Setting up priority queue class

---
+++ src/core/burncode/PriorityQueue.ts
@@ -0,0 +1,101 @@
+/*
+ * Firestarter.io
+ *
+ * Copyright (C) 2021 Blue Ohana, Inc.
+ * All rights reserved.
+ * The information in this software is subject to change without notice and
+ * should not be construed as a commitment by Blue Ohana, Inc.
+ *
+ */
+
+import * as lodash from 'lodash';
+
+interface QueueItem {
+	/**
+	 * The timestamp of the event in the priority queue, also acts as the priority itself
+	 */
+	time: number;
+}
+
+/**
+ * [Priority queue](https://en.wikipedia.org/wiki/Priority_queue) class customized for
+ * FireStarter.
+ *
+ * Adapted from ["Implementation ofPriority Queue in Javascript"](https://www.geeksforgeeks.org/implementation-priority-queue-javascript/)
+ */
+class PriorityQueue {
+	/**
+	 * The items in the queue, should not be accessed directly
+	 */
+	private items: QueueItem[];
+
+	constructor() {
+		this.items = [];
+	}
+
+	/**
+	 * Adds an item to the priority queue in the correct place, based on the item's timestamp
+	 * @param element | A QueueItem object
+	 */
+	enqueue(element: QueueItem) {
+		/**
+		 * Whether or not the new element is going to be added to the end of the queue
+		 */
+		var last = true;
+
+		/**
+		 * The index of the element that already exists in the queue with the same priority as the new element, if it exists
+		 */
+		const preexistingItemIndex = this.items.findIndex(
+			(item) => item.time === element.time
+		);
+
+		/**
+		 * If item already exists in the queue with this time/priority, merge new element
+		 * into existing one
+		 */
+		if (preexistingItemIndex !== -1) {
+			const preexistingItem = { ...this.items[preexistingItemIndex] };
+			this.items[preexistingItemIndex] = lodash.merge(preexistingItem, element);
+		}
+
+		/**
+		 * Loop through existing item and add new element at appropriate place
+		 */
+		for (let i = 0; i < this.items.length; i++) {
+			if (this.items[i].time > element.time) {
+				this.items.splice(i, 0, element);
+				last = false;
+				break;
+			}
+		}
+
+		/**
+		 * If the item was not inserted somewhere by the for loop, it will be last, and should be pushed into the array
+		 */
+		if (last) {
+			this.items.push(element);
+		}
+	}
+
+	/**
+	 * Gets the next item in the priority queue and removes it from the queue
+	 * @returns QueueItem
+	 */
+	next() {
+		if (this.isEmpty()) {
+			return 'Queue is empty!';
+		}
+		return this.items.shift();
+	}
+
+	/**
+	 * Returns true if the queue is empty
+	 * @returns boolean
+	 */
+	isEmpty(): boolean {
+		return this.items.length === 0;
+	}
+}
+
+export default PriorityQueue;
