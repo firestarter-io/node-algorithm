f40b67aaf04439e68f59c29b5ecbfb5fed7009f8
slutske22 <seth.lutske@gmail.com>
2021-09-10 15:51:45 -0700

    Testing PriorityQueue with jest

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+/*
+ * Firestarter.io
+ *
+ * Copyright (C) 2021 Blue Ohana, Inc.
+ * All rights reserved.
+ * The information in this software is subject to change without notice and
+ * should not be construed as a commitment by Blue Ohana, Inc.
+ *
+ */
+
+module.exports = {
+	presets: [
+		['@babel/preset-env', { targets: { node: 'current' } }],
+		'@babel/preset-typescript',
+	],
+};
